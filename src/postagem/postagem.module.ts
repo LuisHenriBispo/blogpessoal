87cf01394d73b1730ae10201d5ac0583b448f44f
Luis Henrique <duimbispo@gmail.com>
2025-07-28 19:40:49 -0300

    Alterando linha desnecessaria

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Postagem } from "src/enitities/postagem.enitity";
-//import { Postagem } from "./entities/postagem.entity";
 
 @Module({
     imports:[TypeOrmModule.forFeature([Postagem])],
