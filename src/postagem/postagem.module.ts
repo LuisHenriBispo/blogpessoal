f2d55d4c228913524042707c901af07117d283ce
Luis Henrique <duimbispo@gmail.com>
2025-07-29 14:10:51 -0300

    criando PostagemModule com controller e service

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Postagem } from "src/enitities/postagem.enitity";
+import { Postagem } from "src/postagem/entities/postagem.entity";
+import { postagemController } from "./controllers/postagem.controller";
+import { PostagemService } from "./services/postagem.service";
 
 @Module({
     imports:[TypeOrmModule.forFeature([Postagem])],
-    controllers: [],
-    providers: [],
+    controllers: [postagemController],
+    providers: [PostagemService],
     exports: [],
 })
 export class PostagemModule{}    
