05d3b4e1fd664016ed0724745bc926909dae5b7b
Luis Henrique <duimbispo@gmail.com>
2025-07-29 13:36:16 -0300

    Arrumando e renomeando o direcionamento das pastas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './enitities/postagem.enitity';
+import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 
 @Module({
