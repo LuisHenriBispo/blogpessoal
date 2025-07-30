7ca3f2890342c5d8bb777b50cd66651f514eb023
Luis Henrique <duimbispo@gmail.com>
2025-07-30 12:07:12 -0300

    Ajustes em Postagem.service

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "src/postagem/entities/postagem.entity";
+import { TemaService } from "../../tema/services/tema.service";
 import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
@@ -8,10 +9,15 @@ export class PostagemService{
     constructor(
         @InjectRepository(Postagem)
         private postagemRepository: Repository<Postagem>,
+        private temaService: TemaService
     ){}
     // buscar todas as informaçoes
     async findAll(): Promise<Postagem[]>{
-        return await this.postagemRepository.find();
+        return await this.postagemRepository.find({
+            relations:{
+                tema: true
+            }
+        });
     }
 
     //busca por id
@@ -19,6 +25,9 @@ export class PostagemService{
         const postagem = await this.postagemRepository.findOne({
             where :{
                 id
+            },
+            relations:{
+                tema: true
             }
         });
 
@@ -33,18 +42,25 @@ export class PostagemService{
         return await this.postagemRepository.find({
             where:{
                 titulo: ILike(`%${titulo}%`)
+            },
+            relations:{
+                tema: true
             }
         })
     }
 
     // cadastrar postagem
     async create(postagem : Postagem): Promise<Postagem>{
+        await this.temaService.findById(postagem.tema.id)
+
         return await this.postagemRepository.save(postagem);
     }
 
     // atualizar postagem
     async update(postagem : Postagem): Promise<Postagem>{
         await this.findbyId(postagem.id)
+
+        await this.temaService.findById(postagem.tema.id)
 
         return await this.postagemRepository.save(postagem);
     }
