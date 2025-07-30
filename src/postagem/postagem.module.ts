398d96d5fbea2234399b4719d5a72d25ec2ba32a
Luis Henrique <duimbispo@gmail.com>
2025-07-30 12:12:22 -0300

    Ajuste na PostagemModule com integração ao TemaModule

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Postagem } from "src/postagem/entities/postagem.entity";
 import { postagemController } from "./controllers/postagem.controller";
 import { PostagemService } from "./services/postagem.service";
+import { TemaModule } from "../tema/tema.module";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Postagem])],
+    imports:[TypeOrmModule.forFeature([Postagem]), TemaModule],
     controllers: [postagemController],
     providers: [PostagemService],
     exports: [],
