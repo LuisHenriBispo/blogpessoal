d52762d096b6a67e072d009468709a560f978482
Luis Henrique <duimbispo@gmail.com>
2025-08-07 11:10:10 -0300

    Configurando a auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,23 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { Bcrypt } from "./bcrypt/bcrypt";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
+import { LocalStrategy } from "./strategy/local.strategy";
+import { UsuarioModule } from "src/usuario/usuario.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants/constants";
 
 @Module({
-    imports: [],
-    providers: [Bcrypt],
-    controllers: [],
+    imports: [forwardRef(() => UsuarioModule),
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: {expiresIn: "1h"},
+        })
+    ],
+    providers: [Bcrypt, AuthService, LocalStrategy],
+    controllers: [AuthController],
     exports: [Bcrypt],
 })
 export class AuthModule {};
