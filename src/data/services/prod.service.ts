c749a08f29ee451e6b6c68f4bfc2e7b438b3c898
Luis Henrique <duimbispo@gmail.com>
2025-08-11 14:10:44 -0300

    Atualização do Deploy - Blog Pessoal

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -8,7 +8,7 @@ export class ProdService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      url: "postgresql://db_blogpessoal_n02n_user:Ah5X67QApllMFUegYAZpE2Jd8w9QpMcW@dpg-d2d0guqdbo4c73c77fs0-a.oregon-postgres.render.com/db_blogpessoal_n02n",
       logging: false,
       dropSchema: false,
       ssl: {
